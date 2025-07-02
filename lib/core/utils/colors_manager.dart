22c116c263cb2c2259cf32ccdf5b421b2c4bfb61
Mohamed Ali <a74498722@gmail.com>
2025-07-02 16:28:26 -0700

    chore: add ColorsManager utility class with color constants

---
+++ lib/core/utils/colors_manager.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+abstract class ColorsManager {
+  static const kWhiteColor = Color(0xffFEF6DB);
+  static const kBrownColor = Color(0xff46322B);
+  static const kOrangeColor = Color(0xffEF9235);
+  static const kGreenColor = Color(0xff558B37);
+  static const kPurpleColor = Color(0xff79359F);
+  static const kSkyColor = Color(0xff50ADC7);
+}
