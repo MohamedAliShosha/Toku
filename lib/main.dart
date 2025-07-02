39a7ef7981cc15f31c3919771dd8e8f5550d7505
Mohamed Ali <a74498722@gmail.com>
2025-07-02 16:23:56 -0700

    chore: add initial files for HomeView and HomeViewBody

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:toku2_app/features/home/presentation/views/home_view.dart';
 
 class TokuApp extends StatelessWidget {
   const TokuApp({super.key});
@@ -7,7 +8,7 @@ class TokuApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(),
+      home: HomeView(),
     );
   }
 }
