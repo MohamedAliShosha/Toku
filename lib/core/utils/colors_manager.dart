979c6fc7a6807431cb6bf73e4827f89daa1142de
Mohamed Ali <a74498722@gmail.com>
2025-07-02 16:29:56 -0700

    refactor: update color constants in ColorsManager and apply kPrimaryColor in HomeView

--- lib/core/utils/colors_manager.dart
+++ lib/core/utils/colors_manager.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 abstract class ColorsManager {
-  static const kWhiteColor = Color(0xffFEF6DB);
+  static const kPrimaryColor = Color(0xffFEF6DB);
   static const kBrownColor = Color(0xff46322B);
   static const kOrangeColor = Color(0xffEF9235);
   static const kGreenColor = Color(0xff558B37);
--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:toku2_app/core/utils/colors_manager.dart';
 import 'package:toku2_app/features/home/presentation/widgets/home_view_body.dart';
 
 class HomeView extends StatelessWidget {
@@ -7,6 +8,7 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
+      backgroundColor: ColorsManager.kPrimaryColor,
       body: HomeViewBody(),
     );
   }
