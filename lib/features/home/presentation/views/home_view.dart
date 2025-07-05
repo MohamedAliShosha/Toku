b5e5ac1b91460ed02670c5b412cf4a929860de27
Mohamed Ali <a74498722@gmail.com>
2025-07-05 14:32:25 -0700

    refactor: update HomeView to use const constructor for HomeViewBody

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -17,7 +17,7 @@ class HomeView extends StatelessWidget {
         backgroundColor: ColorsManager.kBrownColor,
       ),
       backgroundColor: ColorsManager.kPrimaryColor,
-      body: HomeViewBody(),
+      body: const HomeViewBody(),
     );
   }
 }
